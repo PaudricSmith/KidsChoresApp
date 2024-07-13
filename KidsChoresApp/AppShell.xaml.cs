54d680a8449c18fc9f6b4b41a7377e26a3b108dc
Paudric Smith <75310103+PaudricSmith@users.noreply.github.com>
2024-07-13 14:48:31 +0100

    Add project files.

---
+++ KidsChoresApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace KidsChoresApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ KidsChoresApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace KidsChoresApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
