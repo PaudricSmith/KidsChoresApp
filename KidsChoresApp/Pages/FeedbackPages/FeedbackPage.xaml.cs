ccef8d3c70c4dd24d09c1650c9fcdc8a51026a54
Paudric Smith <75310103+PaudricSmith@users.noreply.github.com>
2024-08-07 13:34:13 +0100

    Added Feedback buttons so the user can rate the app on the play store or shown an email address to complain to.

--- KidsChoresApp/Pages/FeedbackPages/FeedbackPage.xaml.cs
+++ KidsChoresApp/Pages/FeedbackPages/FeedbackPage.xaml.cs
@@ -1,10 +1,33 @@
+
+
 namespace KidsChoresApp.Pages.FeedbackPages
 {
     public partial class FeedbackPage : ContentPage
     {
-    	public FeedbackPage()
-    	{
-    		InitializeComponent();
-    	}
+        public FeedbackPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void OnLoveAppClicked(object sender, EventArgs e)
+        {
+            bool answer = await DisplayAlert("Rate this app", "Would you like to rate this app in the Google Play Store?", "Yes", "No");
+            if (answer)
+            {
+                Uri uri = new("https://play.google.com/store/apps/details?id=com.yokee.piano.keyboard");
+                bool isOpened = await Launcher.Default.TryOpenAsync(uri);
+
+                if (!isOpened)
+                {
+                    await DisplayAlert("Error", "Unable to open the URL.", "OK");
+                }
+            }
+        }
+
+        private async void OnDislikeAppClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Contact us", "By email: ooberutuber@gmail.com", "Ok");
+        }
     }
 }
